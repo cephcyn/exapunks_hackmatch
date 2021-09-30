14fa6cd08a097a3464086b354e3fa5dcad0b3f16
cephcyn <cephcyn@gmail.com>
2021-09-29 22:09:52 -0400

    finish parsing board, start solving board

--- gameparser.py
+++ gameparser.py
@@ -50,8 +50,28 @@ def extract_panel(im_prev, im_post):
 
 
 def identify_block(im, ref_block_types):
-    # TODO
-    return 'blue'
+    score_1 = -1
+    block_1 = ''
+    score_2 = -1
+    block_2 = ''
+    for ref_block in ref_block_types:
+        ref_match = cv2.matchTemplate(
+                im,
+                ref_block[2],
+                cv2.TM_CCOEFF_NORMED
+        )
+        ref_score = ref_match[0][0]
+        if ref_score > score_1:
+            score_2 = score_1
+            block_2 = block_1
+            score_1 = ref_score
+            block_1 = ref_block[1]
+        elif ref_score > score_2:
+            score_2 = ref_score
+            block_2 = ref_block[1]
+    if score_1 > 0.4:
+        return block_1
+    return 'none'
 
 
 def read_state(im_prev, im_post, im_draw):
@@ -66,18 +86,27 @@ def read_state(im_prev, im_post, im_draw):
     block_size = int(block_size/7) # there are 7 columns in a game
 
     # Reference for block appearance
-    # Each element is (ref-filename, color, shape)
+    # Each element is (ref-filename, color_shape identifier)
     ref_block_types = [
-            ('ref/block_green.png', 'green', 'diamond'),
-            ('ref/block_blue.png', 'blue', 'grid'),
-            ('ref/block_red.png', 'red', 'excl'),
-            ('ref/block_pink.png', 'pink', 'star'),
-            ('ref/block_yellow.png', 'yellow', 'rows'),
-            ('ref/spike_green.png', 'green_spike', 'diamond_spike'),
-            ('ref/spike_blue.png', 'blue_spike', 'grid_spike'),
-            ('ref/spike_red.png', 'red_spike', 'excl_spike'),
-            ('ref/spike_pink.png', 'pink_spike', 'star_spike'),
-            ('ref/spike_yellow.png', 'yellow_spike', 'rows_spike'),
+            ('ref/b_blu_grid.png', 'b_blu_grid'),
+            ('ref/b_grn_diam.png', 'b_grn_diam'),
+            ('ref/b_pnk_star.png', 'b_pnk_star'),
+            ('ref/b_red_excl.png', 'b_red_excl'),
+            ('ref/b_ylw_rows.png', 'b_ylw_rows'),
+            ('ref/s_blu_grid.png', 's_blu_grid'),
+            ('ref/s_grn_diam.png', 's_grn_diam'),
+            ('ref/s_pnk_star.png', 's_pnk_star'),
+            ('ref/s_red_excl.png', 's_red_excl'),
+            ('ref/s_ylw_rows.png', 's_ylw_rows'),
+    ]
+    # Load in the cv2 reference image parses
+    ref_block_types = [
+            (i[0], i[1], cv2.imread(i[0]))
+            for i in ref_block_types
+    ]
+    ref_block_types = [
+            (i[0], i[1], cv2.resize(i[2], (block_size, block_size)))
+            for i in ref_block_types
     ]
 
     # Search game panel space for the most-fitting block
@@ -86,92 +115,34 @@ def read_state(im_prev, im_post, im_draw):
     p_max_val = 0
     p_max_loc = (0,0)
     for ref_block in ref_block_types:
-        im_ref = cv2.imread(ref_block[0])
-        im_ref = cv2.resize(im_ref, (int(block_size), int(block_size)))
-        im_ref_grey = cv2.cvtColor(im_ref, cv2.COLOR_BGR2GRAY)
         ref_match = cv2.matchTemplate(
                 im_game,
-                im_ref,
+                ref_block[2],
                 cv2.TM_CCOEFF_NORMED
         )
         min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(ref_match)
         if max_val > p_max_val:
             p_max_val = max_val
             p_max_loc = max_loc
-        ref_match = (ref_match * 255).astype('uint8')
-        cv2.imwrite(f'test-{ref_block[1]}.png', ref_match)
+        #ref_match = (ref_match * 255).astype('uint8')
+        #cv2.imwrite(f'test-{ref_block[1]}.png', ref_match)
 
     # Figure out the pin position of that most-fitting block
     p_max_pos = (int(p_max_loc[0]/block_size), int(p_max_loc[1]/block_size))
-    for i_x in range(7):
-        for i_y in range(11):
+
+    # Process each individual block position now
+    board_dims = (7, 10)
+    board = [[] for i in range(board_dims[0])]
+    for i_x in range(board_dims[0]):
+        for i_y in range(board_dims[1]):
             loc_curr = (
                     p_max_loc[0] + (i_x - p_max_pos[0])*block_size,
                     p_max_loc[1] + (i_y - p_max_pos[1])*block_size
             )
             im_game_xy = im_game[loc_curr[1]:loc_curr[1]+block_size, loc_curr[0]:loc_curr[0]+block_size]
             tag = identify_block(im_game_xy, ref_block_types)
-            print(i_x, i_y, loc_curr, tag)
-    # TODO
-
-    """
-    #print(h_max / (w_max / 7))
-    # draw and identify game blocks
-    for col in range(7):
-        for row in range(9):
-            sample_x_lo = x_max + int(w_max*col/7) + int(0.05*w_max/7)
-            sample_x_hi = sample_x_lo + int(0.95*w_max/7)
-            sample_y_lo = y_max + int(w_max*row/7)
-            sample_y_hi = sample_y_lo + int(0.6*w_max/7)
-            # draw block sample pane
-            cv2.rectangle(
-                    panels,
-                    (sample_x_lo, sample_y_lo),
-                    (sample_x_hi, sample_y_hi),
-                    (0,0,255),
-                    3
-            )
-            # average colors within block sample pane
-            sample = after[sample_y_lo:sample_y_hi, sample_x_lo:sample_x_hi]
-            block_color = cv2.mean(sample)
-            cv2.rectangle(
-                    panels,
-                    (sample_x_lo, sample_y_lo),
-                    (sample_x_hi, sample_y_hi),
-                    block_color,
-                    3
-            )
-            # k-means colors within block sample pane
-            kmeans_num = 2
-            sample_pixels = np.float32(sample.reshape(-1, 3))
-            _, labels, palette = cv2.kmeans(
-                    sample_pixels,
-                    kmeans_num, # number of color groups
-                    None, # 'bestLabels'
-                    (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 200, .1), # criteria
-                    10,
-                    cv2.KMEANS_RANDOM_CENTERS # 'flags'
-            )
-            _, counts = np.unique(labels, return_counts=True)
-            # draw each color group
-            for i in range(kmeans_num):
-                gcolor = palette[i] #palette[np.argmax(counts)]
-                gcolor = (int(gcolor[0]), int(gcolor[1]), int(gcolor[2]))
-                cv2.rectangle(
-                        panels,
-                        (sample_x_lo, sample_y_hi-(kmeans_num-i)*10),
-                        (sample_x_hi, sample_y_hi-(kmeans_num-i-1)*10),
-                        gcolor,
-                        -1
-                )
-            # calculate color group dists
-            gdists = [[sum(abs(p - r[0])) for r in BLOCKREF] for p in palette]
-            ideal = np.argmin(gdists)
-            print(
-                    BLOCKREF[ideal%len(BLOCKREF)][1],
-                    [(p[np.argmin(p)], BLOCKREF[np.argmin(p)][1]) for p in gdists]
-            )
-
-    cv2.imwrite(im_draw, im_game)
-    """
+            #cv2.imwrite(f'out-{i_x}-{i_y}-{tag}.png', im_game_xy)
+            #print(i_x, i_y, loc_curr, tag)
+            board[i_x].append(tag)
+    return board
 
--- main.py
+++ main.py
@@ -2,15 +2,29 @@ import time
 import PIL.ImageGrab
 
 import gameparser
+import gamesolver
+
+# force bot to leftmost position
+# TODO
 
 # get screenshot of game state
 # ?Q: how does this interact with multi-monitor setup?
 #im1 = PIL.ImageGrab.grab()
 #im1.save('temp1.png')
-#time.sleep(2)
+#time.sleep(0.5)
 #im2 = PIL.ImageGrab.grab()
 #im2.save('temp2.png')
 
 # parse screenshot of game state
-gameparser.read_state('temp1.png', 'temp2.png', 'temp_panels.png')
+state = gameparser.read_state('temp1.png', 'temp2.png', 'temp_panels.png')
+
+# print game state
+state_cols = len(state)
+state_rows = len(state[0])
+for r in range(state_rows):
+    print(' '.join([c[r] if c[r]!='none' else '----------' for c in state]))
+
+# decide which moves to make
+soln = gamesolver.solve_state(state)
+print(soln)
 
